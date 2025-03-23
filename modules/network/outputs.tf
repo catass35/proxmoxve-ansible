5a925a3b3bfae92f7eb8183ee114b98a5eaa8a7d
catass35 <175985261+catass35@users.noreply.github.com>
2025-03-23 11:12:44 +0100

    Update outputs.tf

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -1,1 +1,15 @@
+output "vlan_id" {
+  value = var.vlan_id
+}
 
+output "vlan_name" {
+  value = var.vlan_name
+}
+
+output "vlan_adress" {
+  value = var.vlan_adress
+}
+
+output "vlan_gateway" {
+  value = var.vlan_gateway
+}
