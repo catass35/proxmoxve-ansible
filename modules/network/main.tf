fbb71736f3a8111680b2408606fa79a96cb6893c
catass35 <175985261+catass35@users.noreply.github.com>
2025-03-23 11:10:26 +0100

    Update main.tf

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,6 +1,10 @@
 resource "proxmox_virtual_environment_network_linux_vlan" var.vlan_ress {
   node_name = "proxmoxve"
+  vlan      = var.vlan_id
   name      = var.vlan_name
+
+  adress    = var.vlan_adress
+  gateway   = var.vlan_gateway
 
   comment = var.vlan_comment
 }
